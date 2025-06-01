6364de66632cbc978fab8d20a44a08e867f96b7a
Akaikumogo <98486553+Akaikumogo@users.noreply.github.com>
2025-06-01 07:37:38 +0500

    ana

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,5 +3,8 @@ export default {
   appId: 'com.example.app',
   appName: 'Nasoslar raqamli boshqaruvi',
   webDir: 'dist',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  server: {
+    androidScheme: 'http' // HTTPS o‘rniga HTTP bo‘ladi
+  }
 };
